52ffb9ab3604d6a72e35599634a63ece20650f9e
fazo96 <fazius2009@gmail.com>
2014-04-26 12:24:31 +0200

    first version

---
+++ README.md
@@ -0,0 +1,19 @@
+# PongMO
+Multiplayer Online pong web-app using node.js, express and socket.io on the
+server side, and Crafty game engine on the client side.
+
+I built this a very small project to learn dynamic web-app developement.
+
+## Usage
+Clone this repo then in the root folder run `npm install`, then in the
+`client` folder run `bower init`. This makes sure you have all the necessary
+client and server dependences installed
+
+Run the app on the server with `node app.js` then point your browser to the
+machine's IP address/hostname on port `3000` (by default).
+
+For example, if you want to run it on your *nix / Mac OS machine you can connect
+to the local machine by navigating your browser to http://localhost:3000/
+
+## License
+MIT
---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+app.use(express.static('./client'));
+
+var p1 = null, p2 = null;
+
+io.sockets.on('connection', function(socket){
+    console.log('connection incoming');
+    if(p1 == null) {
+        p1 = socket;
+        socket.emit('player',1);
+        socket.on('ball',function(data){
+            io.sockets.emit('ball',data);
+            console.log('New ball position: '+data);
+        });
+    }
+    else if(p2 == null) { p2 = socket; socket.emit('player',2);}
+    else socket.emit('player',0);
+    if(p1 != null && p2 != null)
+        io.sockets.emit('status',1); //1 is play, 0 is stop
+
+    socket.on('move',function(pos){
+        io.sockets.emit((socket===p1?'move1':'move2'),pos);
+        console.log('New paddle position: '+pos);
+    })
+});
+
+server.listen(3000);
